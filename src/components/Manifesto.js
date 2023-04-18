e2de4bb34c5f2754db45a48db0c7c96640db3567
Tarek <2b3zab666@gmail.com>
2023-04-19 00:57:57 +0200

    made app resonsive for medium screen

--- src/components/Manifesto.js
+++ src/components/Manifesto.js
@@ -24,13 +24,13 @@ function Manifesto() {
   return (
     <Stack direction={'row'} justifyContent={'space-evenly'} sx={{backgroundColor:'white' ,width:'100%' , height:'102vh'}}>
 
-             <Box sx={{maxWidth:'100%' , height:'100%' , display:'flex' , flexDirection:'row' , justifyContent:'space-evenly'}}>
+             <Box  sx={{maxWidth:'100%' , height:'100%' , display:'flex' , flexDirection:'row' , justifyContent:'space-evenly'}}>
              <motion.div
                     
                     animate={{ opacity: isInViewport ? 1 : 0, x: isInViewport ? 0 : -400 }} // Set animation values based on isInViewport state
                     transition={{ duration: 1 }}
                 >
-                    <Box id='pillars-wraper'  m={2} sx={{width:'300px' , height:'400px' , display:'flex' , flexDirection:'column' ,borderRadius:'15px 15px'  , justifyContent:'center' , gap:'15px',  backgroundImage:
+                    <Box id='pillars-wraper'  m={2} sx={{width:'350px' , height:'400px' , display:'flex' , flexDirection:'column' ,borderRadius:'15px 15px'  , justifyContent:'center' , gap:'15px',  backgroundImage:
                         "linear-gradient(to right, rgb(130, 113, 255), rgb(130, 133, 245))",}}>
                             
                         <Typography variant='h5' sx={{color:'white' , textAlign:'center'}}> <Buildings  size={92} /> </Typography>
@@ -44,7 +44,7 @@ function Manifesto() {
                     animate={{ opacity: isInViewport ? 1 : 0, x: isInViewport ? 0 : -400 , y:240 }}  // Set animation values based on isInViewport state
                     transition={{ duration: 1.5 }}
                 >
-                    <Box id='pillars-wraper'  m={2} sx={{width:'300px' , height:'400px' ,  display:'flex' , flexDirection:'column' ,borderRadius:'15px 15px' , justifyContent:'center' , gap:'15px', backgroundImage:
+                    <Box id='pillars-wraper'  m={2} sx={{width:'350px' , height:'400px' ,  display:'flex' , flexDirection:'column' ,borderRadius:'15px 15px' , justifyContent:'center' , gap:'15px', backgroundImage:
                         "linear-gradient(to right, rgb(130, 113, 255), rgb(130, 133, 245))"}}>
                         <Typography sx={{color:'white' , textAlign:'center'}} variant='h5'> <Handshake size={92} /></Typography>
                         <Typography sx={{color:'white' , textAlign:'center'}} variant='h5'> Introduce the new Anyone mortgage that allows more starters </Typography>
@@ -57,7 +57,7 @@ function Manifesto() {
                     animate={{ opacity: isInViewport ? 1 : 0, x: isInViewport ? 0 : -400  , y:540}} 
                     transition={{ duration: 2}}
                 >
-                    <Box id='pillars-wraper'  m={2} sx={{width:'300px' , height:'400px' , display:'flex' , flexDirection:'column' , borderRadius:'15px 15px' , justifyContent:'center' , gap:'15px', backgroundImage:
+                    <Box id='pillars-wraper'  m={2} sx={{width:'350px' , height:'400px' , display:'flex' , flexDirection:'column' , borderRadius:'15px 15px' , justifyContent:'center' , gap:'15px', backgroundImage:
                         "linear-gradient(to right, rgb(130, 113, 255), rgb(130, 133, 245))" }}>
                          
                          <Typography sx={{color:'white' , textAlign:'center' }} variant='h5'> <Coins size={92} /> </Typography>
